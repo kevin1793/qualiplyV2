bf9e6872653e39d06254b3a6f8a0e2af810bef5e
Kevin Claveria <kevin1793@gmail.com>
2026-01-06 02:04:46 -0600

    Delete functions/setAdmin.js

--- functions/setAdmin.js
+++
@@ -1,6 +0,0 @@
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-admin.auth().setCustomUserClaims("UwrGMOkZfMYL7DrtLic7JmPCiKj2", { admin: true })
-  .then(() => console.log("Admin claim set!"))
-  .catch(console.error);
